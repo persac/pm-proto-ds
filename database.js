2b0c4a1243158c684fc078e6827a7562cc1c80c9
Casper Schipper <casper@pattymorgan.net>
2017-01-26 13:38:59 +0100

    Add scaffolding; styling; webfonts, DB

--- database.js
+++ database.js
@@ -2,7 +2,9 @@ var database = {
   collections: [
     {
       id: "1",
-      title: 'Slecht werk',
+      title: 'Variations of Beige',
+      short: 'We all know beige to be the most boring of colours, but that’s a lie.',
+      description: 'We all know beige to be the most boring of colours, but that’s a lie. You heard it here first. Perhaps your mood just doesn’t swing one way or another. That doesn’t mean you don’t have a super rich emotional life, it just means you don’t have the need to be obnoxiously obvious about it. In this case beige is actually the perfect colour, precisely because of its many and subtle variations. For the connoisseur. So here it is, from cosmic latte to ecru. For you.',
       works: [
         {
           id: "1",
@@ -18,12 +20,68 @@ var database = {
     },
     {
       id: "2",
-      title: 'Goed werk',
+      title: 'If you’re one of those people looking for the ‘photography’ button',
+      short: 'This is it.',
+      description: 'This is it.',
       works: [
         {
           id: "1",
           title: 'Een goed werk',
-          artist: 'Naam Kunstenaar'
+          descript: 'Naam Kunstenaar'
+        },
+        {
+          id: "2",
+          title: 'Een ander goed werk',
+          artist: 'Kunstenaar Naam'
+        }
+      ]
+    },
+    {
+      id: "3",
+      title: 'Big Fillers',
+      short: 'When you have that hollow feeling deep inside you - just fill yourself with colour.',
+      description: 'When you have that hollow feeling deep inside you and you can’t decide if it’s due to a lack of real emotional contact with another human being or because you are hungry and there is no food in the house. Then again, what’s the difference. Just fill yourself with colour.',
+      works: [
+        {
+          id: "1",
+          title: 'Een goed werk',
+          descript: 'Naam Kunstenaar'
+        },
+        {
+          id: "2",
+          title: 'Een ander goed werk',
+          artist: 'Kunstenaar Naam'
+        }
+      ]
+    },
+    {
+      id: "4",
+      title: 'Black is a colour too',
+      short: 'Some people say black is not a colour but to those people we say “Bye”.',
+      description: 'Some people say black is not a colour but to those people we say “Bye”.',
+      works: [
+        {
+          id: "1",
+          title: 'Een goed werk',
+          descript: 'Naam Kunstenaar'
+        },
+        {
+          id: "2",
+          title: 'Een ander goed werk',
+          artist: 'Kunstenaar Naam'
+        }
+      ]
+    },
+    {
+      id: "5",
+      title: 'Super Tight Art',
+      short: 'Getting a little uncomfy digesting too much frivolous frills?',
+      description: 'Getting a little uncomfy digesting too much frivolous frills? Or expecting the Funda photographers to arrive any moment? Pick one of these.',
+      works: [
+        {
+          id: "1",
+          title: 'Een goed werk',
+          descript: 'Naam Kunstenaar'
         },
         {
           id: "2",
