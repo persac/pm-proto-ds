f9949d1bcdd7b275c144d79dc02090a777261e2f
Larix Kortbeek <larixk@gmail.com>
2017-01-26 10:56:36 +0100

    First commit

---
+++ database.js
@@ -0,0 +1,36 @@
+var database = {
+  collections: [
+    {
+      id: "1",
+      title: 'Slecht werk',
+      works: [
+        {
+          id: "1",
+          title: 'Een slecht werk',
+          artist: 'Naam Kunstenaar'
+        },
+        {
+          id: "2",
+          title: 'Een ander slecht werk',
+          artist: 'Kunstenaar Naam'
+        }
+      ]
+    },
+    {
+      id: "2",
+      title: 'Goed werk',
+      works: [
+        {
+          id: "1",
+          title: 'Een goed werk',
+          artist: 'Naam Kunstenaar'
+        },
+        {
+          id: "2",
+          title: 'Een ander goed werk',
+          artist: 'Kunstenaar Naam'
+        }
+      ]
+    }
+  ]
+};
